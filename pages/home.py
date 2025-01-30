3cf6bb0e80d2116a40d5cbd9e016d10c3bad0b8c
Lucas Ramalho <84401571+Lucask22000@users.noreply.github.com>
2025-01-30 11:45:44 -0400

    Add files via upload

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 # Desativa a navegação automática, removendo o menu de páginas
-st.set_page_config(initial_sidebar_state="expanded", page_title="Minha Página")
+st.set_page_config(initial_sidebar_state="collapsed", page_title="Minha Página")
 
 st.title("Home")
 
