020e00e6807773ecb8c69a1f0095b8dd81ac0358
Lucas Ramalho <84401571+Lucask22000@users.noreply.github.com>
2025-01-30 11:23:03 -0400

    Delete pages directory

--- pages/home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import os
-
-# Desativa a navegação automática, removendo o menu de páginas
-st.set_page_config(initial_sidebar_state="expanded", page_title="Minha Página")
-
-st.title("Home")
-
-st.success(f"Login realizado com sucesso!")
